d9fb8fe86d77736a4376ff4b58671f58be9a34ff
Egor <68728836+egorf710@users.noreply.github.com>
2023-03-02 17:57:29 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function openCity(evt, cityName) {
+    var i, tabcontent;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
